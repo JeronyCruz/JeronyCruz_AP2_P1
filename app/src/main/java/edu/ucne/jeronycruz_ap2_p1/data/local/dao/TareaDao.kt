112a7039b82da380cb106c035b99db5d49264a1b
jerony <jerony_cruz@ucne.edu.do>
2025-06-04 18:52:31 -0400

    DAO Realizada.

---
+++ app/src/main/java/edu/ucne/jeronycruz_ap2_p1/data/local/dao/TareaDao.kt
@@ -0,0 +1,26 @@
+package edu.ucne.jeronycruz_ap2_p1.data.local.dao
+
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.jeronycruz_ap2_p1.data.local.entities.TareaEntity
+import kotlinx.coroutines.flow.Flow
+
+interface TareaDao{
+    @Upsert
+    suspend fun save(tarea: TareaEntity)
+
+    @Query("""
+            SELECT *
+            FROM TAREAS
+            WHERE tareaId =:id
+            Limit 1
+    """)
+    suspend fun find(id: Int): TareaEntity?
+
+    @Delete
+    suspend fun delete(tarea: TareaEntity)
+
+    @Query("SELECT * FROM Tareas")
+    fun getAll(): Flow<List<TareaEntity>>
+}
