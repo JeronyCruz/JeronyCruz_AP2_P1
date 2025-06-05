26ca1a1c0c67a5c40fe8ab6f397f7af6e0a1bf63
jerony <jerony_cruz@ucne.edu.do>
2025-06-04 20:28:30 -0400

    Cambio de nombre de archivos.

--- app/src/main/java/edu/ucne/jeronycruz_ap2_p1/presentation/sistema/SistemaScreen.kt
+++
@@ -1,2 +0,0 @@
-package edu.ucne.jeronycruz_ap2_p1.presentation.sistema
-
--- app/src/main/java/edu/ucne/jeronycruz_ap2_p1/presentation/sistema/SistemaUiState.kt
+++
@@ -1,5 +0,0 @@
-package edu.ucne.jeronycruz_ap2_p1.presentation.sistema
-
-data class SistemaUiState (
-    val Id: Int? = null
-)
