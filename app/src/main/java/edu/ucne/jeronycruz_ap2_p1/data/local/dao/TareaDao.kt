8d4af01dfefd7cbe1bf81b947e9c5d2a89e86ac7
jerony <jerony_cruz@ucne.edu.do>
2025-06-04 20:29:06 -0400

    Se agrego el decorador Dao.

--- app/src/main/java/edu/ucne/jeronycruz_ap2_p1/data/local/dao/TareaDao.kt
+++ app/src/main/java/edu/ucne/jeronycruz_ap2_p1/data/local/dao/TareaDao.kt
@@ -1,11 +1,13 @@
 package edu.ucne.jeronycruz_ap2_p1.data.local.dao
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
 import edu.ucne.jeronycruz_ap2_p1.data.local.entities.TareaEntity
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface TareaDao{
     @Upsert
     suspend fun save(tarea: TareaEntity)
