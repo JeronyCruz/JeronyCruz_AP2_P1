7f3e1540d6195a36ae259664576e759e70aa0b57
jerony <jerony_cruz@ucne.edu.do>
2025-06-04 20:33:44 -0400

    Agregando el HostNavigation al MainActivity.

--- app/src/main/java/edu/ucne/jeronycruz_ap2_p1/MainActivity.kt
+++ app/src/main/java/edu/ucne/jeronycruz_ap2_p1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -24,7 +25,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             JeronyCruz_AP2_P1Theme {
                 val nav = rememberNavController()
-                HostNavigation(nav)
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Box(
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        HostNavigation(nav)
+                    }
+
+                }
             }
         }
     }
